3bf0535e727421e7e85dbb667c0729d8ef6dac9e
Xavier Grand <grand.xavier@gmail.com>
2014-03-06 01:53:28 -0800

    Add multipleQueries

--- src/main/java/com/algolia/search/saas/APIClient.java
+++ src/main/java/com/algolia/search/saas/APIClient.java
@@ -466,4 +466,43 @@ public class APIClient {
         }
         throw new AlgoliaException("Hosts unreachable");
     }
+    
+    static public class IndexQuery {
+    	private String index;
+    	private Query query;
+    	public IndexQuery(String index, Query q)  {
+    		this.index = index;
+    		this.query = q;
+    	}
+		public String getIndex() {
+			return index;
+		}
+		public void setIndex(String index) {
+			this.index = index;
+		}
+		public Query getQuery() {
+			return query;
+		}
+		public void setQuery(Query query) {
+			this.query = query;
+		}
+    }
+    /**
+     * This method allows to query multiple indexes with one API call
+     */
+    public JSONObject multipleQueries(List<IndexQuery> queries) throws AlgoliaException {
+    		try {
+    			JSONArray requests = new JSONArray();
+    			for (IndexQuery indexQuery : queries) {
+    				String paramsString = indexQuery.getQuery().getQueryString();
+				requests.put(new JSONObject().put("indexName", indexQuery.getIndex()).put("params", paramsString));
+    			}
+				JSONObject body = new JSONObject().put("requests", requests);
+				return postRequest("/1/indexes/*/queries", body.toString());
+			} catch (JSONException e) {
+				new AlgoliaException(e.getMessage());
+			}
+    		return null;
+    }
+    
 }
