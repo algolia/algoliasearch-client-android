f20e54d332cc73193c1896ad93b172cacfd307dc
Sylvain UTARD <sylvain.utard@gmail.com>
2015-03-03 23:40:09 +0100

    Move to Maven Central

--- release.sh
+++ release.sh
@@ -1,9 +1,3 @@
 #!/bin/sh
 mvn -Darguments="-DskipTests" release:prepare
-mvn -DskipTests release:perform
-cp -R target/checkout/repository .
-git add .
-echo "Your now need to perform:"
-echo 'git ci -a -m "Release X.Y.Z"'
-echo 'git pull'
-echo 'git push'
+mvn -Darguments="-DskipTests" release:perform
