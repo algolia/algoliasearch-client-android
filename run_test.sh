3f786018d8397a69b19d93784e125d8ee77e6b38
Paul-Louis NECH <PLNech@users.noreply.github.com>
2016-04-11 16:08:42 +0200

    Travis: Narrower retry use (only on gradle task) (#68)

--- run_test.sh
+++ run_test.sh
@@ -6,6 +6,10 @@ set -o pipefail
 FILE=algoliasearch/src/test/java/com/algolia/search/saas/Helpers.java
 export FILE
 
+if [ "$TRAVIS" = "true" ]; then
+    RETRY="travis_retry" # If on travis, use travis_retry
+fi
+
 if ! [[ $TRAVIS_JOB_NUMBER && ${TRAVIS_JOB_NUMBER-_} ]]; then
     echo "/!\ TRAVIS_JOB_NUMBER is not set."
     TRAVIS_JOB_NUMBER=$RANDOM.$RANDOM
@@ -13,5 +17,5 @@ fi
 
 echo "Running Android test..."
 ./setup_tests.sh
-./gradlew testOnlineRelease
+$RETRY ./gradlew testOnlineRelease
 ./teardown_tests.sh
