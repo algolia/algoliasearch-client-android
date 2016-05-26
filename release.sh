fc51bd2c9182efcde3dd2a230618378b1b93529e
Clément Le Provost <clement.leprovost@algolia.com>
2016-05-26 16:13:30 +0200

    Update Maven release script

--- release.sh
+++ release.sh
@@ -2,6 +2,8 @@
 
 set -eo pipefail
 
+SELF_ROOT=$(cd $(dirname "$0") && pwd)
+
 function call_sed(){
 PATTERN="$1"
 FILENAME="$2"
@@ -14,8 +16,8 @@ else
 fi
 }
 
-FILE_BUILD_GRADLE="algoliasearch/common.gradle"
-FILE_API_CLIENT="algoliasearch/src/main/java/com/algolia/search/saas/Client.java"
+FILE_BUILD_GRADLE="$SELF_ROOT/algoliasearch/common.gradle"
+FILE_API_CLIENT="$SELF_ROOT/algoliasearch/src/main/java/com/algolia/search/saas/Client.java"
 
 if [ $# -ne 1 ]; then
     echo "$0 | A script to release new versions automatically"
@@ -25,15 +27,24 @@ fi
 
 VERSION_CODE=$1
 
-echo "Updating version code to $VERSION_CODE and committing..."
+echo "Updating version code to $VERSION_CODE..."
 call_sed "s/(PUBLISH_VERSION =) '.*'/\1 '$VERSION_CODE'/" $FILE_BUILD_GRADLE
 call_sed "s/(private final static String version =) \".*\"/\1 \"$VERSION_CODE\"/" $FILE_API_CLIENT
 
-git add $FILE_BUILD_GRADLE $FILE_API_CLIENT && git commit -m "Release $VERSION_CODE"
-git --no-pager show --name-status --format="short"
+git diff $FILE_BUILD_GRADLE $FILE_API_CLIENT
 
-echo "Updating artifacts..."
-./gradlew uploadArchives 1>/dev/null
+echo "Uploading flavor 'online'..."
+$SELF_ROOT/select-flavor.sh online
+$SELF_ROOT/gradlew uploadArchives 1>/dev/null
+
+echo "Uploading flavor 'offline'"
+$SELF_ROOT/select-flavor.sh online
+$SELF_ROOT/gradlew uploadArchives 1>/dev/null
 
 echo "Success! Closing and releasing new version..."
-./gradlew closeAndPromoteRepository 1>/dev/null
+$SELF_ROOT/gradlew closeAndPromoteRepository 1>/dev/null
+
+# Revert flavor to original.
+git checkout $SELF_ROOT/algoliasearch/build.gradle
+
+echo "IMPORANT: Remember to git commit & tag & push"
