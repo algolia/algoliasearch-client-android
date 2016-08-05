1623571631078e14003278c48480d69d1a2f95db
Paul-Louis NECH <pln@algolia.com>
2016-08-05 10:41:59 +0200

    Version 3.4.0

--- algoliasearch/src/main/java/com/algolia/search/saas/Client.java
+++ algoliasearch/src/main/java/com/algolia/search/saas/Client.java
@@ -63,7 +63,7 @@ import java.util.zip.GZIPInputStream;
  * </p>
  */
 public class Client {
-    private final static String version = "3.3.0";
+    private final static String version = "3.4.0";
 
     /**
      * The user agents as a raw string. This is what is passed in request headers.
