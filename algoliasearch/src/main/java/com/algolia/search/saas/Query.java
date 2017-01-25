862f28ad208455b2a0d08fbc1cb0f28012a6468e
Paul-Louis NECH <pln@algolia.com>
2017-01-25 15:40:09 +0100

    [feat] facetingAfterDistinct: better warning about parameter

--- algoliasearch/src/main/java/com/algolia/search/saas/Query.java
+++ algoliasearch/src/main/java/com/algolia/search/saas/Query.java
@@ -380,9 +380,10 @@ public class Query extends AbstractQuery {
     }
 
     /**
-     * Force faceting to be applied after de-duplication. See https://www.algolia.com/doc/rest-api/search/#facetingafterdistinct
+     * Force faceting to be applied after de-duplication. Please check <a href="https://www.algolia.com/doc/rest-api/search/#facetingafterdistinct">documentation</a> for consequences and limitations
      *
      * @param enabled if {@code true}, facets will be computed after de-duplication is applied.
+     * @see <a href="https://www.algolia.com/doc/rest-api/search/#facetingafterdistinct">facetingAfterDistinct's documentation</a>
      */
     public @NonNull Query setFacetingAfterDistinct(Boolean enabled) {
         return set(KEY_FACETING_AFTER_DISTINCT, enabled);
