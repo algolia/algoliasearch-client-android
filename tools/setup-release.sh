3a992236dbd4443c20b9026cdda0722f449ee25b
Paul-Louis NECH <pln@algolia.com>
2018-04-12 14:46:00 -0700

    chore(setup-release): refactor, drop eventual open repository before release

--- tools/setup-release.sh
+++ tools/setup-release.sh
@@ -1,9 +1,39 @@
 #! /bin/sh
-GRADLE_FOLDER=~/.gradle/
-GRADLE_PROPERTIES=$GRADLE_FOLDER/gradle.properties
-KEY_PATH=`pwd`/tools/bitrise-secret.key
+maybe_install_gpg() {
+    # Install gnupgV2 if missing
+    if ! gpg --version > /dev/null; then
+        echo "No gnupg* found. Installing gnupg2..."
+        apt install gnupg2
+    fi
+}
 
-add_signing() {
+maybe_install_conv() {
+    # Install conventional-changelog-cli if missing
+    npm list -g conventional-changelog-cli > /dev/null || npm install -g conventional-changelog-cli
+}
+
+setup_gradle_signing() {
+    # Setup gradle properties for gpg signing artifacts
+    GRADLE_FOLDER=~/.gradle/
+    GRADLE_PROPERTIES=$GRADLE_FOLDER/gradle.properties
+    KEY_PATH=`pwd`/tools/bitrise-secret.key
+
+    if [ ! -d $GRADLE_FOLDER ]
+    then
+        mkdir $GRADLE_FOLDER
+    fi
+
+    if [ -e $GRADLE_PROPERTIES ]
+    then
+        if grep -q "signing." $GRADLE_PROPERTIES
+        then
+            echo "The gradle properties already have a signing profile, leaving untouched."
+            return 0
+        fi
+    else
+        touch $GRADLE_PROPERTIES
+    fi
+
     echo "Adding bitrise key to gradle properties..."
     cat > $GRADLE_PROPERTIES << EOF
 signing.keyId=9719DC41
@@ -12,33 +42,62 @@ signing.secretKeyRingFile=$KEY_PATH
 EOF
     echo "New gradle properties:"
     cat $GRADLE_PROPERTIES
+    gpg --import $KEY_PATH
 }
 
-# Install gnupg2 if missing
-if ! gpg --version > /dev/null; then
-    echo "No gnupg* found. Installing gnupg2..."
-    apt install gnupg2
-fi
+# Maven drop repository handling TODO: Remove if alternative https://issues.sonatype.org/browse/OSSRH-39124
+drop_open_repo() {
+    echo "Creating minimal pom to use nexus maven plugin"
+    cat > pom.xml << EOF
+<project>
+    <groupId>com.algolia</groupId>
+    <artifactId>algoliasearch-android</artifactId>
+    <version>0.0.0</version>
+    <modelVersion>4.0.0</modelVersion>
+    <build>
+        <plugins>
+            <plugin>
+                <groupId>org.sonatype.plugins</groupId>
+                <artifactId>nexus-staging-maven-plugin</artifactId>
+                <version>1.6.6</version>
+                <extensions>true</extensions>
+                <configuration>
+                    <serverId>nexus</serverId>
+                    <nexusUrl>https://oss.sonatype.org/</nexusUrl>
+                </configuration>
+            </plugin>
+        </plugins>
+    </build>
+</project>
+EOF
 
-# Install conventional-changelog-cli if missing
-npm list -g conventional-changelog-cli || npm install -g conventional-changelog-cli
+    echo "Creating local maven settings"
+    cat > mvn-settings.xml << EOF
+<settings>
+    <servers>
+        <server>
+            <id>nexus</id>
+            <username>$NEXUS_USERNAME</username>
+            <password>$NEXUS_PASSWORD</password>
+        </server>
+    </servers>
+</settings>
+EOF
 
-# Setup gradle properties for gpg signing artifacts
-if [ ! -d $GRADLE_FOLDER ]
-then
-    mkdir $GRADLE_FOLDER
-fi
-
-if [ -e $GRADLE_PROPERTIES ]
-then
-    if grep -q "signing." $GRADLE_PROPERTIES
-    then
-        echo "The gradle properties already have a signing profile, leaving untouched."
-        exit 0
+    MVN_REPO_ID=`mvn --settings mvn-settings.xml nexus-staging:rc-list | grep comalgolia | cut -d' ' -f 2`
+    if test -z $MVN_REPO_ID; then
+        echo "No repository was currently open."
+    else
+        echo "Existing repository to close: $MVN_REPO_ID"
+        mvn nexus-staging:drop -DstagingRepositoryId=$MVN_REPO_ID
     fi
-else
-    touch $GRADLE_PROPERTIES
-fi
 
-add_signing
-gpg --import $KEY_PATH
+    echo "Removing maven-related files..."
+    rm pom.xml
+    rm mvn-settings.xml
+}
+
+maybe_install_gpg
+maybe_install_conv
+drop_open_repo
+setup_gradle_signing
