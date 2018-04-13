ffed4b3f776e5d83a1dcd3432b5c5849aa0f3687
Paul-Louis NECH <pln@algolia.com>
2018-04-12 17:40:00 -0700

    chore(release): sleep before posting release

--- release.sh
+++ release.sh
@@ -104,4 +104,4 @@ git commit -m "chore(release): Version $VERSION_CODE [ci skip]"
 echo "Release complete! Pushing to GitHub"
 git tag "$VERSION_CODE" -s -m "v$VERSION_CODE"
 git push origin $VERSION_CODE HEAD
-
+sleep 5 # To avoid race condition when posting release while GitHub processes tag
